ade1610c9804aa8f74ec59dba2df93eda66a3ec2
viniciusgati <viniciusgati@gmail.com>
2011-02-02 14:50:16 -0200

    novas classes iniciais

--- lib/entidades/cliente.rb
+++ lib/entidades/cliente.rb
@@ -1,59 +1,60 @@
+require 'parametros'
+
 class Cliente
-  # Dados do Cliente
-    # - Nome/Razão Social (obrigatorio)
-    attr_accessor :razao_social
+  # - Nome/Razão Social (obrigatorio)
+  attr_accessor :razao_social
 
-    # - Tipo de Documento (obrigatorio) - default CNPJ - TIPOS_DOCUMENTO
-    attr_accessor :tipo_documento
+  # - Tipo de Documento (obrigatorio) - default CNPJ - TIPOS_DOCUMENTO
+  attr_accessor :tipo_documento
 
-    # - Numero do Documento (obrigatorio)
-    attr_accessor :numero_documento
+  # - Numero do Documento (obrigatorio)
+  attr_accessor :numero_documento
 
-    # - Inscricao Estadual
-    attr_accessor :inscricao_estadual
+  # - Inscricao Estadual
+  attr_accessor :inscricao_estadual
 
-    # - Inscricao SUFRAMA
-    attr_accessor :inscricao_suframa
+  # - Inscricao SUFRAMA
+  attr_accessor :inscricao_suframa
 
-    # - Isento do ICMS (Sim/Nao)
-    attr_accessor :isento_icms
+  # - Isento do ICMS (Sim/Nao)
+  attr_accessor :isento_icms
 
-    # Endereco
-    # - Logradouro (obrigatorio)
-    attr_accessor :endereco_logradouro
+  # Endereco
+  # - Logradouro (obrigatorio)
+  attr_accessor :endereco_logradouro
 
-    # - Numero (obrigatorio)
-    attr_accessor :endereco_numero
+  # - Numero (obrigatorio)
+  attr_accessor :endereco_numero
 
-    # - Complemento
-    attr_accessor :endereco_complemento
+  # - Complemento
+  attr_accessor :endereco_complemento
 
-    # - Bairro (obrigatorio)
-    attr_accessor :endereco_bairro
+  # - Bairro (obrigatorio)
+  attr_accessor :endereco_bairro
 
-    # - CEP
-    attr_accessor :endereco_cep
+  # - CEP
+  attr_accessor :endereco_cep
 
-    # - Pais (seleciona de lista)
-    attr_accessor :endereco_pais
+  # - Pais (seleciona de lista)
+  attr_accessor :endereco_pais
 
-    # - UF (obrigatorio)
-    attr_accessor :endereco_uf
+  # - UF (obrigatorio)
+  attr_accessor :endereco_uf
 
-    # - Municipio (obrigatorio)
-    attr_accessor :endereco_municipio
+  # - Municipio (obrigatorio)
+  attr_accessor :endereco_municipio
 
-    # - Telefone
-    attr_accessor :endereco_telefone
+  # - Telefone
+  attr_accessor :endereco_telefone
 
   def initialize
-    isento_icms   = false
-    endereco_pais = CODIGO_BRASIL
-    tipo_documento= 'CNPJ'
+    @isento_icms   = false
+    @endereco_pais = Parametros::CODIGO_BRASIL
+    @tipo_documento= 'CNPJ'
   end
 
   def to_s
-    "#{tipo_documento numero_documento}"
+    "#{@tipo_documento} #{@numero_documento}"
   end
 
 end
--- lib/entidades/produto.rb
+++ lib/entidades/produto.rb
@@ -1,59 +1,59 @@
 # E provavel que esta entidade sera descartada, porque?
 class Produto
-    # - Descricao (obrigatorio)
-    attr_accessor :descricao
+  # - Descricao (obrigatorio)
+  attr_accessor :descricao
 
-    # - Codigo (obrigatorio) - nao pode ser alterado quando em edicao
-    attr_accessor :codigo
+  # - Codigo (obrigatorio) - nao pode ser alterado quando em edicao
+  attr_accessor :codigo
 
-    # - EAN
-    attr_accessor :ean
+  # - EAN
+  attr_accessor :ean
 
-    # - EAN Unid. Tributavel
-    attr_accessor :ean_unidade_tributavel
+  # - EAN Unid. Tributavel
+  attr_accessor :ean_unidade_tributavel
 
-    # - EX TIPI
-    attr_accessor :ex_tipi
+  # - EX TIPI
+  attr_accessor :ex_tipi
 
-    # - Genero
-    attr_accessor :genero
+  # - Genero
+  attr_accessor :genero
 
-    # - NCM
-    attr_accessor :ncm
+  # - NCM
+  attr_accessor :ncm
 
-    # - Unid. Com.
-    attr_accessor :unidade_comercial
+  # - Unid. Com.
+  attr_accessor :unidade_comercial
 
-    # - Valor Unitario Com.
-    attr_accessor :valor_unitario_comercial
+  # - Valor Unitario Com.
+  attr_accessor :valor_unitario_comercial
 
-    # - Unid. Trib.
-    attr_accessor :unidade_tributavel
+  # - Unid. Trib.
+  attr_accessor :unidade_tributavel
 
-    # - Qtd. Trib.
-    attr_accessor :quantidade_tributavel
+  # - Qtd. Trib.
+  attr_accessor :quantidade_tributavel
 
-    # - Valor Unitario Trib.
-    attr_accessor :valor_unitario_tributavel
+  # - Valor Unitario Trib.
+  attr_accessor :valor_unitario_tributavel
 
-    # Impostos
-    # - ICMS (lista 1 para * / ManyToManyField)
-    attr_accessor :icms
+  # Impostos
+  # - ICMS (lista 1 para * / ManyToManyField)
+  attr_accessor :icms
 
-    # - IPI
-    # - Classe de Enquadramento (cigarros e bebidas)
-    attr_accessor :ipi_classe_enquadramento
+  # - IPI
+  # - Classe de Enquadramento (cigarros e bebidas)
+  attr_accessor :ipi_classe_enquadramento
 
-    # - Codigo de Enquadramento Legal
-    attr_accessor :ipi_codigo_enquadramento_legal
+  # - Codigo de Enquadramento Legal
+  attr_accessor :ipi_codigo_enquadramento_legal
 
-    # - CNPJ do Produtor
-    attr_accessor :ipi_cnpj_produtor
+  # - CNPJ do Produtor
+  attr_accessor :ipi_cnpj_produtor
 
-def initialize
-  icms = []
-end
-  
+  def initialize
+    @icms = []
+  end
+
 end
 
 
